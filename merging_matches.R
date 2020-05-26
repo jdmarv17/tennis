a87529a5c84c1bd5c89470faa7e799501cb9e159
Josh Marvald <joshmarvald@Joshs-MacBook-Pro.local>
2020-05-26 15:38:00 -0400

    merged match data

---
+++ merging_matches.R
@@ -0,0 +1,77 @@
+
+atp2010 <- read_csv("data/atp_matches_2010.csv")
+atp2011 <- read_csv("data/atp_matches_2011.csv")
+atp2012 <- read_csv("data/atp_matches_2012.csv")
+atp2013 <- read_csv("data/atp_matches_2013.csv")
+atp2014 <- read_csv("data/atp_matches_2014.csv")
+atp2015 <- read_csv("data/atp_matches_2015.csv")
+atp2016 <- read_csv("data/atp_matches_2016.csv")
+atp2017 <- read_csv("data/atp_matches_2017.csv")
+atp2018 <- read_csv("data/atp_matches_2018.csv")
+atp2019 <- read_csv("data/atp_matches_2019.csv")
+atp2020 <- read_csv("data/atp_matches_2020.csv")
+
+atp2019$winner_seed <- as.numeric(as.character(atp2019$winner_seed))
+atp2020$winner_seed <- as.numeric(as.character(atp2020$winner_seed))
+atp2019$loser_seed <- as.numeric(as.character(atp2019$loser_seed))
+atp2020$loser_seed <- as.numeric(as.character(atp2020$loser_seed))
+
+atp_decade <- bind_rows(atp2010, atp2011, atp2012, atp2013,atp2014, atp2015, atp2016, atp2017, 
+                        atp2018, atp2019, atp2020)
+
+atp_decade <-
+  atp_decade %>%
+  mutate(`w_serveperc` = w_1stIn/w_svpt) %>%
+  mutate(`l_serveperc` = l_1stIn/l_svpt)
+
+gs_decade <-
+  atp_decade %>%
+  filter(tourney_name == "Australian Open" | tourney_name == "Wimbledon" | 
+           tourney_name == "Roland Garros" | tourney_name == "US Open")
+
+# player column
+gs_decade_long <-
+  gs_decade %>%
+  pivot_longer(c(winner_name, loser_name), names_to = "win_loss", values_to = "player")
+
+# matches lost
+losers <- gs_decade_long %>%
+  select(-starts_with("w_")) %>%
+  filter(win_loss == "loser_name")
+
+# matches won
+winners <- gs_decade_long %>%
+  select(-starts_with("l_")) %>%
+  filter(win_loss == "winner_name")
+
+gs_matches_final <- bind_rows(losers, winners)
+
+# make just one variable for wether the player won
+gs_matches_final <-
+  gs_matches_final %>%
+  mutate(win = case_when(
+    win_loss == "loser_name" ~ 0,
+    win_loss == "winner_name" ~ 1
+  )) 
+
+# one variable for first serve percentage
+gs_matches_final <-
+  gs_matches_final %>%
+  mutate(first_serve = case_when(
+    win == 0 ~ l_serveperc,
+    win == 1 ~ w_serveperc
+  ))
+
+gs_matches_final %>%
+  filter(player == "Roger Federer" | player == "Rafael Nadal" | player == "Novak Djokovic") %>%
+  ggplot(., aes(x = first_serve, y = win, color = player)) +
+  stat_smooth(method = "glm", method.args = c("binomial"), se = F) +
+  theme_bw()
+
+
+
+
+
+
+
+
